b9208e6e7b61e342eccb6c2fe797334a17e3fd7b
Samuel Sena <samuelcardososena19@gmail.com>
2022-06-02 13:25:42 -0300

    inicio

---
+++ public/preload.js
@@ -0,0 +1,15 @@
+setTimeout(function(){
+
+    var desable = document.getElementById("overlay");
+    
+    desable.style="display:none"
+
+}, 1500);
+
+setTimeout(function(){
+
+    var desable = document.getElementById("overlay2");
+    
+    desable.style="display:none"
+
+}, 1500);
